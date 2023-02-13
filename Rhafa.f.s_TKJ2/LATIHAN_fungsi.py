05caf3e92924f4ae2e782ad48b02fde79ff42b87
FAHMI56 <111480318+FAHMI56@users.noreply.github.com>
2023-02-13 10:41:01 +0700

    Add files via upload

---
+++ Rhafa.f.s_TKJ2/LATIHAN_fungsi.py
@@ -0,0 +1,45 @@
+'''FUNGSI PAD PYTHON DIDEFINISIKAN DENGAN DEF'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("Assalammu'alaikum !! akhi ukthiy")
+
+salam()
+salam()
+salam()
+
+'''buatlah sebuah fungsi versi kalian'''
+
+
+def pergi():
+    print("aku pergi dulu")
+
+pergi()
+
+
+'''fungsi demgan parameter'''
+'''def nama_fungsi(p1,p2)'''
+
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya", nama)
+
+
+
+say_hi("jawir")
+say_hi("reog")
+
+'''buat lah fungsi dengan 3 parameter'''
+
+def say_hi(nama,umur,kelas):
+    pass
+    print("halo nama saya",nama)
+    print("umur saya", umur)
+    print("kelas saya",kelas)
+
+say_hi("rusdi","3","5 TKJ 3")
+
+print("\n\n=========== BATAS ============\n\n")
+
+say_hi("ardi","4","7 TKJ 6")
